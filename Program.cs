06715f003b810ed040dde1f6bf4229b0e86ea073
macgibbons <macgibbons@gmail.com>
2020-02-27 14:02:31 -0600

    practice declaring variables and printing using interpolation

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyApp2 {
+    class Program {
+        static void Main (string[] args) {
+
+            // defining variables - C# is type specific
+            string message = "hello world";
+            int numberOfTacos = 10;
+            bool hasMeat = false;
+            List<string> names = new List<string> () { "Adam", "Brenda", "Rose" };
+
+            // console logging variables
+            Console.WriteLine (message);
+            Console.WriteLine (numberOfTacos);
+            Console.WriteLine (hasMeat);
+
+            // string concacting 
+            Console.WriteLine (message + " " + "I'd like " + numberOfTacos + " tacos. Meat? " + hasMeat);
+
+            // string interpolation 
+            Console.WriteLine ($"{message} I'd like {numberOfTacos} tacos. Meat? {hasMeat}");
+
+        }
+    }
+}
