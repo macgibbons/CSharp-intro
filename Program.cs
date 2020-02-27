0a6f6ec756d9e1e34afbc9e57828a4c5ca637e2e
macgibbons <macgibbons@gmail.com>
2020-02-27 14:47:54 -0600

    practicing loops and conditionals

--- Program.cs
+++ Program.cs
@@ -11,6 +11,18 @@ namespace MyApp2 {
             bool hasMeat = false;
             List<string> names = new List<string> () { "Adam", "Brenda", "Rose" };
 
+            // How to loop over a List of Strings
+
+            // for loops
+            for (int i = 0; i < names.Count; i++) {
+                Console.WriteLine (names[i]);
+            }
+
+            // foreach loops
+            foreach (string name in names) {
+                Console.WriteLine (name);
+            }
+
             // console logging variables
             Console.WriteLine (message);
             Console.WriteLine (numberOfTacos);
@@ -21,6 +33,15 @@ namespace MyApp2 {
 
             // string interpolation 
             Console.WriteLine ($"{message} I'd like {numberOfTacos} tacos. Meat? {hasMeat}");
+
+            // conditional statements
+            if (numberOfTacos > 5) {
+                Console.WriteLine ("Oh Boy, That's a lotta tacos!");
+            } else if (numberOfTacos < 3) {
+                Console.WriteLine ("Level up your taco game.");
+            } else {
+                Console.WriteLine ("Sounds like a good number o' tacos.");
+            }
 
         }
     }
